08c4d14e93803807d6524f3c1d9d1456d3bfc94c
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2018-10-04 22:01:27 +0300

    remove comented out line

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,5 +7,4 @@ $route['pages/create'] = 'pages/create';
 $route['categories/create'] = 'categories/create';
 $route['categories/posts/(:any)'] = 'categories/posts/$1';
 $route['404_override'] = '';
-//$route['pages'] = 'pages';
 $route['translate_uri_dashes'] = FALSE;
