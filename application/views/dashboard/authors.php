32548dfb9ec79dbcec440d6903ee81053ceed91e
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2018-10-20 11:30:15 +0300

    Reindent some code

--- application/views/dashboard/authors.php
+++ application/views/dashboard/authors.php
@@ -20,47 +20,47 @@
                 </thead>
                 <tbody>
                   <?php foreach ($authors as $author): ?>
-                    <tr id="<?php echo $author->id ?>" class="row m-0">
-                      <td class="w-5"><?php echo $author->id ?></td>
-                      <td class="w-20"><?php echo $author->first_name . " " . $author->last_name; ?></td>
-                      <td class="w-25"><?php echo $author->email; ?></td>
-                      <td class="w-20"><?php echo nice_date($author->register_date, 'M d, Y') . ' at '.  nice_date($author->register_date, 'H:i:s'); ?></td>
-                      <td class="w-10 status-column">
-                        <?php if ($author->active == 1) {
-                          echo '<span class="text-success">' . 'Enabled' . '</span>';
+                  <tr id="<?php echo $author->id ?>" class="row m-0">
+                    <td class="w-5"><?php echo $author->id ?></td>
+                    <td class="w-20"><?php echo $author->first_name . " " . $author->last_name; ?></td>
+                    <td class="w-25"><?php echo $author->email; ?></td>
+                    <td class="w-20"><?php echo nice_date($author->register_date, 'M d, Y') . ' at '.  nice_date($author->register_date, 'H:i:s'); ?></td>
+                    <td class="w-10 status-column">
+                      <?php if ($author->active == 1) {
+                        echo '<span class="text-success">' . 'Enabled' . '</span>';
                         } else {
-                          echo '<span class="text-danger">' . 'Disabled' . '</span>';
+                        echo '<span class="text-danger">' . 'Disabled' . '</span>';
                         }
                         ?>
-                      </td>
-                      <td class="w-10 text-center"><?php echo $author->is_admin; ?></td>
-                      <td class="text-center activate-column d-inline-block w-10">
-                        <?php if ($author->is_admin == 0): ?>
-                          <div class="btn-group">                 
-                            <?php if ($author->active == 1): ?>
-                              <a href="<?php echo base_url('dashboard/users/deactivate/'. $author->id); ?>" title="Deactivate" class="btn btn-sm btn-success state-change" data-role="deactivate" data-id="<?php echo $author->id ?>"><span class="glyphicon glyphicon-ban-circle"></span> Disable</a>
-                              <?php else: ?>
-                                <a href="<?php echo base_url('dashboard/users/activate/' . $author->id); ?>" title="Activate" class="btn btn-sm btn-success state-change" data-role="activate" data-id="<?php echo $author->id ?>"><span class="glyphicon glyphicon-ok"></span> Enable</a>
-                              <?php endif; ?>
-                              <a href="<?php echo base_url('dashboard/users/delete/' . $author->id); ?>" title="Delete" class="delete-user btn btn-sm btn-success"><span class="glyphicon glyphicon-trash"></span> Delete</a>
-                            </div>
-                            <?php else: ?>
-                              <a href="#" class="btn btn-sm btn-block btn-success disabled">No actions</a>
-                            <?php endif; ?>
-                          </td>
-                        </tr>
-                      <?php endforeach ?>
-                    </tbody>
-                  </table>
-                  <div class="pagination-container text-center">
-                    <?php //echo $this->pagination->create_links(); ?>
-                  </div>
-                  <?php else: ?>
-                    <p class="text-center">No records to display</p>
-                  <?php endif ?>
-                </div>
+                    </td>
+                    <td class="w-10 text-center"><?php echo $author->is_admin; ?></td>
+                    <td class="text-center activate-column d-inline-block w-10">
+                      <?php if ($author->is_admin == 0): ?>
+                      <div class="btn-group">                 
+                        <?php if ($author->active == 1): ?>
+                        <a href="<?php echo base_url('dashboard/users/deactivate/'. $author->id); ?>" title="Deactivate" class="btn btn-sm btn-success state-change" data-role="deactivate" data-id="<?php echo $author->id ?>"><span class="glyphicon glyphicon-ban-circle"></span> Disable</a>
+                        <?php else: ?>
+                        <a href="<?php echo base_url('dashboard/users/activate/' . $author->id); ?>" title="Activate" class="btn btn-sm btn-success state-change" data-role="activate" data-id="<?php echo $author->id ?>"><span class="glyphicon glyphicon-ok"></span> Enable</a>
+                        <?php endif; ?>
+                        <a href="<?php echo base_url('dashboard/users/delete/' . $author->id); ?>" title="Delete" class="delete-user btn btn-sm btn-success"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+                      </div>
+                      <?php else: ?>
+                      <a href="#" class="btn btn-sm btn-block btn-success disabled">No actions</a>
+                      <?php endif; ?>
+                    </td>
+                  </tr>
+                  <?php endforeach ?>
+                </tbody>
+              </table>
+              <div class="pagination-container text-center">
+                <?php //echo $this->pagination->create_links(); ?>
               </div>
-            </div>
-          </main>
+            <?php else: ?>
+              <p class="text-center">No records to display</p>
+            <?php endif ?>
+          </div>
         </div>
       </div>
+    </main>
+  </div>
+</div>
--- application/views/dashboard/categories.php
+++ application/views/dashboard/categories.php
@@ -17,28 +17,28 @@
                 </thead>
                 <tbody>
                   <?php foreach ($categories as $category): ?>
-                    <tr id="<?php echo $category->id; ?>">
-                      <td><?php echo $category->id; ?></td>
-                      <td><?php echo $category->name; ?></td>
-                      <td class="text-center">
-                        <div class="btn-group btn-group-sm" role="group">
-                          <?php if($this->session->userdata('user_is_admin')) : ?>
-                            <a href="<?php echo base_url('dashboard/categories/edit/') . $category->id; ?>" class="btn btn-success"><i class="fa fa-pencil-square-o"></i> Edit</a>
-                            <a href="<?php echo base_url('dashboard/categories/delete/') . $category->id; ?>" class="delete-category btn btn-success"><i class="fa fa-trash"></i> Delete</a>
-                            <?php else: ?>
-                              <a href="#" class="btn btn-success disabled">Edit</a>
-                              <a href="#" class="btn btn-success disabled">Delete</a>
-                            <?php endif; ?>
-                          </div>
-                        </td>
-                      </tr>
-                    <?php endforeach ?>
-                  </tbody>
-                </table>            
-              </div>
+                  <tr id="<?php echo $category->id; ?>">
+                    <td><?php echo $category->id; ?></td>
+                    <td><?php echo $category->name; ?></td>
+                    <td class="text-center">
+                      <div class="btn-group btn-group-sm" role="group">
+                        <?php if($this->session->userdata('user_is_admin')) : ?>
+                        <a href="<?php echo base_url('dashboard/categories/edit/') . $category->id; ?>" class="btn btn-success"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        <a href="<?php echo base_url('dashboard/categories/delete/') . $category->id; ?>" class="delete-category btn btn-success"><i class="fa fa-trash"></i> Delete</a>
+                        <?php else: ?>
+                        <a href="#" class="btn btn-success disabled">Edit</a>
+                        <a href="#" class="btn btn-success disabled">Delete</a>
+                        <?php endif; ?>
+                      </div>
+                    </td>
+                  </tr>
+                  <?php endforeach ?>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   </div>
+</div>
