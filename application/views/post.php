80f30af599756c277c9779ae64f28a8163c45ef1
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2020-04-29 22:52:59 +0300

    restyle the meta info section og the single post view

--- application/views/post.php
+++ application/views/post.php
@@ -3,20 +3,20 @@
     <main class="content" id="post_content">
       <h2 class="post-title display-4"><?php echo $post->title; ?></h2>
       <div class="row post-meta">
-        <div class="left-half col-sm-8">
+        <div class="left-half col-sm-12 col-xl-8">
           
           <?php $author_image = isset($post->avatar) && $post->avatar !== '' ? $post->avatar : 'default-avatar.png'; ?>
 
           <span class="author">
             <a href="<?php echo base_url('posts/byauthor/') . $post->author_id; ?>">
-              <img src="<?php echo base_url('assets/img/authors/') . $author_image; ?>" alt="<?php echo $post->first_name . ' ' . $post->last_name; ?>" class="rounded-circle">
+              <img src="<?php echo base_url('assets/img/authors/') . $author_image; ?>" alt="<?php echo $post->first_name . ' ' . $post->last_name; ?>" class="rounded-circle <?php echo isset($post->avatar) && $post->avatar !== '' ? '' : 'bg-light border' ?>">
               <span class="pl-1"><?php echo $post->first_name . " " . $post->last_name; ?></span>
             </a>
             </span> <strong>&#183;</strong> <span class="date"><?php echo nice_date($post->created_at, 'M d, Y'); ?></span>
         </div>
-        <div class="right-half col-sm-4">
+        <div class="right-half col-sm-12 col-xl-4">
           <?php $comments_count = count($comments); $comments_status = $comments_count > 0 ? $comments_count . ' comments': "No comments"; ?>
-          <a class="comments" id="comments_status" href="#" title="<?php echo $comments_status; ?>"><i class="fa fa-comments"></i> <?php echo $comments_status; ?></a> <strong>&#183;</strong>
+          <a class="comments" id="comments_status" href="#" title="<?php echo $comments_status; ?>"><i class="fa fa-comments mr-1"></i> <?php echo $comments_status; ?></a> <strong>&#183;</strong>
           <a href="#" id="print_post"><i class="fa fa-print"></i> Print</a>
         </div>
       </div>
