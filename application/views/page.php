2924436314ae7db92a0cb38aca79da6c477218e2
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2020-05-01 23:26:22 +0300

    typo fix

--- application/views/page.php
+++ application/views/page.php
@@ -1,7 +1,7 @@
 <div class="container fluid-fixed">
   <div class="content-wrapper">
     <main class="content">
-      <h2 class="post-title display-4"><?php echo $page->title; ?></h2>
+      <h2 class="page-title display-4"><?php echo $page->title; ?></h2>
       <div class="page-content">
         <?php echo $page->content; ?>
       </div>
