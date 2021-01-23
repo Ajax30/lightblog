6d484f77787348fc82234e12f507931d69eddf7c
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-01-23 23:25:50 +0200

    Hide image slider

--- application/models/Static_model.php
+++ application/models/Static_model.php
@@ -8,7 +8,7 @@ class Static_model extends CI_Model {
 		$data['tagline'] = "A simple blog application made with Codeigniter 3";
 		$data['company_name'] = "My Company";
 		$data['company_email'] = "company@domain.com";
-		$data['is_featured'] = true;
+		$data['is_featured'] = false;
 		$data['is_ckeditor'] = false;
 		$data['is_cookieconsent'] = true;
 		return $data;
