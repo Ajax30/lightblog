173ba91e0c43d0be044751e3446d313ef4e93422
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-03-01 00:01:49 +0200

    hide data

--- application/controllers/Passwordreset.php
+++ application/controllers/Passwordreset.php
@@ -71,8 +71,8 @@ class Passwordreset extends CI_Controller {
         // Loading the Email library
         $config['protocol']  = 'smtp';
 				$config['smtp_host'] = 'ssl://smtp.googlemail.com';
-				$config['smtp_user'] = 'razvan.zamfir80@gmail.com';
-				$config['smtp_pass'] = 'myGoop123@';
+				$config['smtp_user'] = 'myaddress@gmail.com';
+				$config['smtp_pass'] = '*****';
 				$config['smtp_port'] = 465;
 				$config['charset']   = 'utf-8';
 				$config['mailtype']  = 'html';
